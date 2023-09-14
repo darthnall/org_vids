ea3772f261c127023de22b77eb39bc07d7ca400a
darthnall <blakenall@proton.me>
2023-09-14 00:07:06 -0500

    Fixed filepath bug

--- org_vids/__init__.py
+++ org_vids/__init__.py
@@ -2,8 +2,8 @@ import os
 
 def sort():
     client_name = input("Client name: ")
-    files = os.listdir("./input")
-    for file in files:
-        ext = file.split(".")[1]
-        new_name = f"{client_name}-{file.index}.{ext}"
-        os.rename(os.path.abspath(file), os.path.abspath(f".output/{new_name}"))
+    files = os.listdir("./org_vids/input")
+    for index, file in enumerate(files):
+        ext = files[0].split(".")[1]
+        new_filename = f"{client_name}-{index}.{ext}"
+        os.rename(f"./org_vids/input/{file}", f"./org_vids/output/{new_filename}")
