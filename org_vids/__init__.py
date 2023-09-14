21ab8f9be615f68e529578149ac3e3247da4c690
darthnall <blakenall@proton.me>
2023-09-14 00:32:36 -0500

    Changed prompt

--- org_vids/__init__.py
+++ org_vids/__init__.py
@@ -1,9 +1,9 @@
 import os
 
 def sort():
-    client_name = input("Client name: ")
+    new_filename = input("The new files will appear as:\nNew name, hyphen, number\n\nNew filename: ")
     files = os.listdir("./org_vids/input")
     for index, file in enumerate(files):
         ext = files[0].split(".")[1]
-        new_filename = f"{client_name}-{index}.{ext}"
+        new_filename = f"{new_filename}-{index}.{ext}"
         os.rename(f"./org_vids/input/{file}", f"./org_vids/output/{new_filename}")
--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 from org_vids import sort
+import os
+
+try:
+    os.mkdir("./output")
+    os.mkdir("./input")
+except FileExistsError:
+    pass
 
 if __name__ == "__main__":
     sort()
