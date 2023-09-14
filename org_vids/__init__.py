822d5239136b210ff9334c9c189688d9e0f9158f
darthnall <blakenall@proton.me>
2023-09-14 10:51:05 -0500

    Fixed path bug again

--- org_vids/__init__.py
+++ org_vids/__init__.py
@@ -1,9 +1,15 @@
 import os
 
 def sort():
-    client_name = input("Client name: ")
-    files = os.listdir("./input")
-    for file in files:
+    client_name = input("""
+        Input new filename for files.\n
+        Files are formatted as follows:\n
+
+        <NEW_FILENAME>-<INDEX>.<EXTENSION>\n\n
+
+        New Filename: """)
+    files = os.listdir("./org_vids/input")
+    for index, file in enumerate(files):
         ext = file.split(".")[1]
-        new_name = f"{client_name}-{file.index}.{ext}"
-        os.rename(os.path.abspath(file), os.path.abspath(f".output/{new_name}"))
+        new_name = f"{client_name}-{index}.{ext}"
+        os.rename(os.path.abspath(f"./org_vids/input/{file}"), os.path.abspath(f"./org_vids/output/{new_name}"))
--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 from org_vids import sort
+import os
+
+try:
+    os.mkdir("./org_vids/output")
+    os.mkdir("./org_vids/input")
+except FileExistsError:
+    pass
 
 if __name__ == "__main__":
     sort()
